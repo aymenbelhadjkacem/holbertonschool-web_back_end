4734b53b55ac7a7db4c8aae746164f5c152dd982
aymenbelhadjkacem <aymenbelhadjkacem>
2022-03-10 09:08:31 -0500

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""take a list and return a sum"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Take a list and return a sum"""
+    return sum(input_list)
