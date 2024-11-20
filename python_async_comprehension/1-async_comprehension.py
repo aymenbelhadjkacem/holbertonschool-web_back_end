0dbe212573a384571dac81011c2ac964b04ecec9
aymenbelhadjkacem <77977533+aymenbelhadjkacem@users.noreply.github.com>
2024-11-20 11:50:15 +0100

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""
-_summary_
-"""
-
-import asyncio
+''' Async Comprehension '''
 from typing import List
+Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """_summary_
-    Async Comprehensions
-    Returns:
-        list[float]: random numbers
-    """
-    return [i async for i in async_generator()]
+async def async_comprehension() -> Vector:
+    ''' Function that return a list '''
+    Final = [y async for y in async_generator()]
+    return Final
