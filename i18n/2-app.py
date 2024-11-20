9af68488712fc2bc12bc09fc37abf3e61ebdd86b
aymenbelhadjkacem <77977533+aymenbelhadjkacem@users.noreply.github.com>
2024-11-20 14:52:58 +0100

    Update 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""
-Get locale from request
-"""
-from flask import Flask, render_template, request
-from flask_babel import Babel
-from babel import negotiate_locale
+''' Flask app '''
 
-app = Flask(__name__, template_folder='templates')
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """Config Class"""
+class Config:
+    ''' App config '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,20 +18,17 @@ class Config(object):
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """basic index"""
+@babel.localeselector
+def get_locale():
+    ''' return best languages '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
     return render_template('2-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """Returns the best matched locale based on the languages
-    requested by the client"""
-    return negotiate_locale('LANGUAGES',
-                            request.accept_languages.best_match(
-                                app.config['LANGUAGES']))
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
